2a4c5aa2478b03de43afb8a6bf3e29c3cd9ea27c
Md Mazharul Islam Farhad <mazharul383all@gmail.com>
2022-02-17 17:17:59 +0600

    claculate func

--- js/index.js
+++ js/index.js
@@ -2,13 +2,24 @@ const incomeField = document.getElementById("incomeField");
 const foodField = document.getElementById("foodField");
 const rentField = document.getElementById("rentField");
 const clothField = document.getElementById("clothField");
-const saveField = document.getElementById("incomeField");
+const saveField = document.getElementById("saveField");
+const saveError = document.getElementById("saveError");
+const saveAmount = document.getElementById("saveAmount");
+const remainAmount = document.getElementById("remainAmount");
 
 let incomeValue = 0;
 let foodValue = 0;
 let rentValue = 0;
-let colothValue = 0;
+let clothValue = 0;
 let saveValue = 0;
+
+let incomeError = "";
+let foodError = "";
+let rentError = "";
+let clothError = "";
+
+let expensesValue = 0;
+let balanceValue = 0;
 
 incomeField.addEventListener("keyup", function (event) {
   getInput(event.target, event.target.value, "income");
@@ -23,21 +34,68 @@ rentField.addEventListener("keyup", function (event) {
 clothField.addEventListener("keyup", function (event) {
   getInput(event.target, event.target.value, "cloth");
 });
+saveField.addEventListener("keyup", function (event) {
+  getSave(event.target.value);
+});
 
 function getInput(elment, value, type) {
   eval(type + "Value" + " = " + value + ";");
+  eval(type + "Error" + " = " + null);
 
   let parentOfField = elment.parentNode;
 
   let nodelist = parentOfField.childNodes;
 
-  if (typeof incomeValue != "number") {
+  if (typeof eval(type + "Value") != "number") {
     nodelist[5].innerText = "Must type number";
-  } else if (incomeValue < 0) {
+    eval(type + "Error" + " = " + 1 + ";");
+  } else if (eval(type + "Value") < 0) {
     nodelist[5].innerText = "Must type positive";
+    eval(type + "Error" + " = " + 1 + ";");
   } else {
     nodelist[5].innerText = "";
     eval(type + "Value" + " = " + value + ";");
+    eval(type + "Error" + " = " + null);
   }
   console.log(eval(type + "Value" + " = " + value + ";"));
+
+  console.log(
+    incomeValue + " " + foodValue + " " + rentValue + " " + clothValue
+  );
+  console.log(
+    incomeError + " " + foodError + " " + rentError + " " + clothError
+  );
+}
+const expenses = document.getElementById("expenses");
+const balance = document.getElementById("balance");
+
+const calculateBtn = document
+  .getElementById("calculate")
+  .addEventListener("click", function () {
+    if (
+      incomeError != 1 &&
+      foodError != 1 &&
+      rentError != 1 &&
+      clothError != 1
+    ) {
+      expensesValue = foodValue + rentValue + clothValue;
+      console.log(expensesValue);
+
+      expenses.innerText = expensesValue;
+      balanceValue = incomeValue - expensesValue;
+      console.log(balanceValue);
+      balance.innerText = balanceValue;
+    } else {
+      expenses.innerText = "put valid input";
+      balance.innerText = "put valid input";
+    }
+  });
+
+function getSave(e) {
+  console.log(typeof e);
+
+  if (e < 0) {
+    saveError.innerText = "Must be positive";
+  } else {
+  }
 }
