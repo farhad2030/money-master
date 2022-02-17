76ad774c57d82bce9bfa1976e3798ab4e8afe966
Md Mazharul Islam Farhad <mazharul383all@gmail.com>
2022-02-17 17:58:25 +0600

    save function

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@ const rentField = document.getElementById("rentField");
 const clothField = document.getElementById("clothField");
 const saveField = document.getElementById("saveField");
 const saveError = document.getElementById("saveError");
-const saveAmount = document.getElementById("saveAmount");
-const remainAmount = document.getElementById("remainAmount");
+const saveAmountBox = document.getElementById("saveAmountBox");
+const remainAmountBox = document.getElementById("remainAmountBox");
+const saveBtn = document.getElementById("saveBtn");
 
 let incomeValue = 0;
 let foodValue = 0;
@@ -20,6 +21,10 @@ let clothError = "";
 
 let expensesValue = 0;
 let balanceValue = 0;
+
+let saveAmount = 0;
+let remainAmount = 0;
+let saveErrorValue = null;
 
 incomeField.addEventListener("keyup", function (event) {
   getInput(event.target, event.target.value, "income");
@@ -37,9 +42,12 @@ clothField.addEventListener("keyup", function (event) {
 saveField.addEventListener("keyup", function (event) {
   getSave(event.target.value);
 });
+saveBtn.addEventListener("click", function (event) {
+  calculateSave(event.target.value);
+});
 
 function getInput(elment, value, type) {
-  eval(type + "Value" + " = " + value + ";");
+  eval(type + "Value" + " = " + value);
   eval(type + "Error" + " = " + null);
 
   let parentOfField = elment.parentNode;
@@ -90,12 +98,56 @@ const calculateBtn = document
       balance.innerText = "put valid input";
     }
   });
-
+// validate save field
 function getSave(e) {
   console.log(typeof e);
-
+  console.log(balanceValue);
+  e = parseFloat(e);
   if (e < 0) {
     saveError.innerText = "Must be positive";
+    saveErrorValue = 1;
   } else {
+    if (balanceValue <= 0) {
+      saveError.innerText = "No more balance";
+      saveErrorValue = 1;
+    } else {
+      console.log("dfds");
+
+      saveError.innerText = "";
+      saveValue = e;
+      saveErrorValue = null;
+    }
+  }
+}
+
+// save calculate
+function calculateSave() {
+  if (
+    saveErrorValue == 1 &&
+    incomeError == 1 &&
+    foodError == 1 &&
+    rentError == 1 &&
+    clothError == 1
+  ) {
+    saveError.innerText = "No more balance";
+    saveAmountBox.innerText = "No more balance";
+    remainAmountBox.innerText = "No more balance";
+  } else {
+    saveAmount = (incomeValue * saveValue) / 100;
+
+    console.log(saveAmount);
+
+    if (saveAmount >= balanceValue) {
+      saveError.innerText = "No more balance";
+      saveAmountBox.innerText = "No more balance";
+      remainAmountBox.innerText = "No more balance";
+    } else {
+      // console.log(typeof balance);
+      // console.log(typeof saveAmount);
+      saveAmountBox.innerText = saveAmount;
+      remainAmount = balanceValue - saveAmount;
+
+      remainAmountBox.innerText = remainAmount;
+    }
   }
 }
